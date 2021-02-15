244bbea7be30e58e188ea7433a6eed01a51bb9c4
e.patti <e.patti.guest@posteitaliane.it>
2021-02-15 17:55:07 +0100

    initial commit

---
+++ src/main/java/soap/service/CountryEndpoint.java
@@ -0,0 +1,33 @@
+package soap.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import soap_service.GetCountryRequest;
+import soap_service.GetCountryResponse;
+
+
+
+@Endpoint
+public class CountryEndpoint {
+	private static final String NAMESPACE_URI = "http://soap-service";
+
+	private CountryRepository countryRepository;
+
+	@Autowired
+	public CountryEndpoint(CountryRepository countryRepository) {
+		this.countryRepository = countryRepository;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+	@ResponsePayload
+	public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+		GetCountryResponse response = new GetCountryResponse();
+		response.setCountry(countryRepository.findCountry(request.getName()));
+
+		return response;
+	}
+}
